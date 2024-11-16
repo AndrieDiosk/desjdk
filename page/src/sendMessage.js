15837a1f142a657a05bbffc5d1d7560af751e340
hiden-github <149639661+hiden-github@users.noreply.github.com>
2024-11-16 13:55:25 +0800

    Update sendMessage.js

--- page/src/sendMessage.js
+++ page/src/sendMessage.js
@@ -4,17 +4,20 @@ module.exports = function (event) {
   return function sendMessage(text, senderID) {
     const recipientID = senderID || event.senderID;
 
+    // Function to split the message into chunks
     function splitMessage(text) {
       const maxLength = 2000;
       const messages = [];
       let remainingText = text;
 
       while (remainingText.length > maxLength) {
+        // Find the last newline character within the maxLength limit
         let splitIndex = remainingText.lastIndexOf("\n", maxLength);
+
         if (splitIndex === -1) {
           splitIndex = maxLength;
         } else {
-          splitIndex += 1;
+          splitIndex += 1; // Include the newline character in the split
         }
 
         messages.push(remainingText.slice(0, splitIndex).trim());
@@ -27,6 +30,7 @@ module.exports = function (event) {
 
     const messages = splitMessage(text);
 
+    // Process each message chunk
     const sendPromises = messages.map(message => {
       const form = {
         recipient: { id: recipientID },
@@ -34,13 +38,10 @@ module.exports = function (event) {
         messaging_type: "RESPONSE",
       };
 
-     if (text.quick_replies) {
-form.text.quick_replies = text.quick_replies;
-     }
-
       return Graph(form);
     });
 
+    // Return a single promise that resolves when all messages are sent
     return Promise.all(sendPromises)
       .then(results => results)
       .catch(err => {
@@ -49,7 +50,9 @@ form.text.quick_replies = text.quick_replies;
   };
 
   function Graph(form) {
-    return axios.post(`https://graph.facebook.com/v20.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+    return axios
+      .post(
+        `https://graph.facebook.com/v20.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
         form
       )
       .then((res) => res.data)
@@ -58,3 +61,6 @@ form.text.quick_replies = text.quick_replies;
       });
   }
 };
+
+// If an error occurs please contact @YanMaglinte
+// FB: https://www.facebook.com/yandeva.me
